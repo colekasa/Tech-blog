6e88db5c8c60111037df2186778671065c97f177
Cole <ce.kasabian@gmail.com>
2023-03-31 21:23:05 -0700

    added models

--- models/Comments.js
+++ models/Comments.js
@@ -1,1 +1,48 @@
-//description, userid, postid, and time/date 
+//description, userid, postid, and time/date
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "posts",
+        key: "id",
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "project",
+  }
+);
+
+
+module.exports = Comments;
--- models/Posts.js
+++ models/Posts.js
@@ -1,2 +1,44 @@
-//we need description, title, userid, time and date 
+//we need description, title, userid, time and date
 //look up how to add timestamps in sequelize
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
+);
+
+module.exports = Posts;
